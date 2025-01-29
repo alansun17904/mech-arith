ad8f3df6d12361bb8e67967a6061002bc25c757b
Alan Sun <asun17904@gmail.com>
2025-01-29 14:13:22 -0500

    additional benchmarks for precedence

---
+++ src/scripts/bool-instable.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+python3 logic.baseline_bool phi-1_5 phi-1_5-only-not --no_and --no_or \
+	--allow_parentheses --num 1000 --depth 6
+
+python3 logic.baseline_bool phi-1_5 phi-1_5-only-and --no_not --no_or \
+	--allow_parentheses --num 1000 --depth 6
+
+python3 logic.baseline_bool phi-1_5 phi-1_5-only-or --no_not --no_and \
+	--allow_parentheses --num 1000 --depth 6
+
+python3 logic.baseline_bool phi-1_5 no-paran-phi-1_5-only-not --no_and --no_or \
+	--num 1000 --depth 6
+
+python3 logic.baseline_bool phi-1_5 no-paran-phi-1_5-only-and --no_not --no_or \
+	--num 1000 --depth 6
+
+python3 logic.baseline_bool phi-1_5 no-paran-phi-1_5-only-or --no_not --no_and \
+	--num 1000 --depth 6
+
+python3 logic.baseline_bool phi-1_5 phi-1_5-no-paran --allow_parentheses \
+	--num 1000 --depth 6
+# python3 logic.baseline_bool phi-1_5 phi-1_5-not-and --no_and --no_or \
+# 	--allow_parentheses --num 1000 --depth 6
+
+# python3 logic.baseline_bool phi-1_5 phi-1_5-only-not --no_not --no_or \
+# 	--allow_parentheses --num 1000 --depth 6
+
+# python3 logic.baseline_bool phi-1_5 phi-1_5-only-not --no_not --no_and \
+# 	--allow_parentheses --num 1000 --depth 6
