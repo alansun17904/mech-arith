f7067fe4ac0a9c8c5be79c1464757a1c547ab8cd
Alan Sun <asun17904@gmail.com>
2025-01-29 15:04:25 -0500

    pareto frontier scripts for boolean circuits

---
+++ src/scripts/bool_pareto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for expl in {3..9}; do
+  for depth in {1..6}; do
+    python3 -m logic.bool_pareto phi-1_5 "logic/data/phi-1_5-bool-$expl$depth" "logic/data/phi-1_5-bool-$expl$depth" --allow_parentheses \
+      --batch_size 16 --exp_length $expl --depth $depth --num 1000
+  done
+done
