boolean discovery added kl metric and prompting fixed dyck

src/logic/baseline_bool.py
@@ -35,7 +35,7 @@ def parse_args():
     parser.add_argument("--allow_parentheses", action="store_true")
     parser.add_argument("--batch_size", type=int, help="batch size", default=32)
     parser.add_argument(
-        "--depth", type=int, help="maximum parenthetical depth", default=3
+        "--depth", type=int, help="maximum parenthetical depth", default=6
     )
     parser.add_argument("--seed", type=int, help="random seed", default=42)
     parser.add_argument("--num", type=int, help="num problems", default=1000)
@@ -57,7 +57,7 @@ def eval_pass(model, dataloader):
         last_logits_tf = logits[:, -1, [f_token, t_token]]
         predict = torch.argmax(last_logits_tf, dim=1)
         correct += sum([labels[i] == predict[i] for i in range(len(labels))])
-    return correct / total, total
+    return (correct / total).item(), total
 
 
 if __name__ == "__main__":
@@ -102,7 +102,7 @@ if __name__ == "__main__":
                 i,
                 "expressions",
                 "accuracy:",
-                d[(i, dp)][0].item(),
+                d[(i, dp)][0],
                 "depth",
                 dp,
                 "total",

src/logic/bool_circuit_discovery.py
@@ -112,6 +112,28 @@ def metric(
         score = torch.mean(score)
     return score
 
+def kl_metric(
+    model,
+    logits,
+    clean_logits,
+    input_length,
+    labels,
+    t_token,
+    f_token,
+    mean=True,
+    loss=True,
+):
+    last_token_logits = logits[:, -1, :]
+    last_token_clean_logits = clean_logits[:, -1, :]
+
+    probs = torch.softmax(last_token_logits, dim=-1)
+    clean_probs = torch.softmax(last_token_clean_logits, dim=-1)
+    results = F.kl_div(
+        probs.log(), clean_probs.log(), log_target=True, reduction="none"
+    ).mean(-1)
+    return results.mean() if mean else results
+
+
 
 def correct_probs(model, logits, clean_logits, input_length, labels, t_token, f_token):
     last_logit_tf = logits[:, -1, [f_token, t_token]]
@@ -178,7 +200,7 @@ if __name__ == "__main__":
         model,
         g,
         dataloader,
-        partial(metric, model, t_token=t_token, f_token=f_token),
+        partial(kl_metric, model, t_token=t_token, f_token=f_token),
         method="EAP-IG",
         ig_steps=5,
     )